e896c6eafc1625a800845977c5676ac6b270add6
shivamsk00 <shivamsk00>
2025-04-30 09:16:55 +0530

    add react-native-vector-icon

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Entypo from "react-native-vector-icons/Entypo"
 
 const App = () => {
   return (
     <View style={{ flex: 1, backgroundColor: 'gray', alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ fontSize: 40 }}>App</Text>
+      <Entypo name="home" size={50} />
     </View>
   )
 }
